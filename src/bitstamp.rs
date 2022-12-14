e8d84834f3ad6f225428c2bdcb30538e3674070c
Ilja Khabarov <ilja.khabarov@gmail.com>
2022-12-15 00:02:35 +0700

    Update Bitstamp approach for new sink scheme

--- src/bitstamp.rs
+++ src/bitstamp.rs
@@ -1,7 +1,19 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
-use futures_util::{future, pin_mut, SinkExt, StreamExt};
+use crate::data::{Orderbook, PriceLevel};
+use futures_util::{
+    future, pin_mut,
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt,
+};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::sync::{Arc, RwLock};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{
+    connect_async, tungstenite::protocol::Message, MaybeTlsStream, WebSocketStream,
+};
+
+use core::str::FromStr;
 
 #[derive(Deserialize, Serialize)]
 struct OrderUpdate(Vec<String>);
@@ -15,7 +27,6 @@ const BITSTAMP_SUBSCRIBE: &str = r#"{
     }
 }
 "#;
-
 
 #[derive(Deserialize, Serialize)]
 struct BitstampData {
@@ -32,6 +43,93 @@ struct BitstampResponse {
     event: String,
 }
 
+struct Sink;
+impl Sink {
+    pub fn handle_update(&mut self, asks: Vec<OrderUpdate>, bids: Vec<OrderUpdate>) -> () {
+        println!("Sinked!: {} {}", asks.len(), bids.len())
+    }
+}
+
+pub struct BitstampClient {
+    sink: Sink,
+}
+
+pub type AsyncWriteChannel = SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>;
+pub type AsyncReadChannel = SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>;
+
+impl BitstampClient {
+    pub fn new() -> Self {
+        Self { sink: Sink {} }
+    }
+    pub fn arc(self) -> Arc<RwLock<Self>> {
+        Arc::new(RwLock::new(self))
+    }
+
+    pub async fn init_connectors() -> (AsyncWriteChannel, AsyncReadChannel) {
+        let connect_addr = BITSTAMP_ADDR;
+        let url = url::Url::parse(&connect_addr).unwrap();
+
+        let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+        println!("Connection successful");
+
+        ws_stream.split()
+    }
+
+    pub async fn subscribe(read: &mut AsyncReadChannel, write: &mut AsyncWriteChannel) -> () {
+        let msg: Message = Message::text(BITSTAMP_SUBSCRIBE);
+        write.send(msg).await.unwrap();
+        println!("Subscribe sent");
+        let response = read.next().await;
+        match response {
+            Some(Ok(m)) => {
+                tokio::io::stdout().write_all(&m.into_data()).await.unwrap();
+                tokio::io::stdout()
+                    .write_all("\n".as_bytes())
+                    .await
+                    .unwrap();
+            }
+            _ => {
+                tokio::io::stdout()
+                    .write_all("Failed to receive response to subscription\n".as_bytes())
+                    .await
+                    .unwrap();
+            }
+        }
+    }
+
+    pub async fn run(state: Arc<RwLock<BitstampClient>>, read: AsyncReadChannel) -> () {
+        read.for_each(|message| async {
+            state.write().unwrap().handle_message(message.unwrap());
+        })
+        .await;
+    }
+
+    fn handle_message(&mut self, message: Message) -> () {
+        let data = message
+            .into_text()
+            .expect("Failed to convert Message to String");
+
+        if let Ok(update) = serde_json::from_str::<BitstampResponse>(&data) {
+            println!("--- Data");
+            let asks_update = update.data.asks;
+            let bids_update = update.data.bids;
+
+            self.sink.handle_update(asks_update, bids_update);
+        } else {
+            println!("Unknown response. Ignoring: {}", data);
+        }
+    }
+}
+
+pub async fn do_bitstamp_v2() {
+    let asks = Orderbook::new().arc();
+    let bids = Orderbook::new().arc();
+    let client = BitstampClient::new().arc();
+    let (mut write, mut read) = BitstampClient::init_connectors().await;
+    BitstampClient::subscribe(&mut read, &mut write).await;
+    BitstampClient::run(client, read).await;
+}
+
 pub async fn do_bitstamp() {
     let connect_addr = BITSTAMP_ADDR;
     let url = url::Url::parse(&connect_addr).unwrap();
@@ -39,7 +137,7 @@ pub async fn do_bitstamp() {
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     println!("Connection successful");
 
-    let (mut write  , read) = ws_stream.split();
+    let (mut write, read) = ws_stream.split();
 
     let msg: Message = Message::text(BITSTAMP_SUBSCRIBE);
     write.send(msg).await.unwrap();
@@ -48,23 +146,36 @@ pub async fn do_bitstamp() {
     let ws_to_stdout = {
         read.for_each(|message| async {
             let data = message.unwrap().into_data();
+            tokio::io::stdout()
+                .write_all(format!("{}\n", String::from_utf8(data.clone()).unwrap()).as_bytes())
+                .await
+                .unwrap();
             let update = serde_json::from_slice::<BitstampResponse>(&data);
             match update {
                 Ok(u) => {
-                    tokio::io::stdout().write_all("Ok\n".as_bytes()).await.unwrap();
+                    tokio::io::stdout()
+                        .write_all("Ok\n".as_bytes())
+                        .await
+                        .unwrap();
                     if u.data.bids.len() != 100 || u.data.asks.len() != 100 {
-                        tokio::io::stdout().write_all("NOT 100\n".as_bytes()).await.unwrap();
+                        tokio::io::stdout()
+                            .write_all("NOT 100\n".as_bytes())
+                            .await
+                            .unwrap();
                     }
                 }
-                Err(e) => {
-                    tokio::io::stdout().write_all(format!("Not Ok: {:?}\n", e).as_bytes()).await.unwrap()
-                }
+                Err(e) => tokio::io::stdout()
+                    .write_all(format!("Not Ok: {:?}\n", e).as_bytes())
+                    .await
+                    .unwrap(),
             };
             tokio::io::stdout().write_all(&data).await.unwrap();
-            tokio::io::stdout().write_all("\n".as_bytes()).await.unwrap();
+            tokio::io::stdout()
+                .write_all("\n".as_bytes())
+                .await
+                .unwrap();
         })
     };
 
     ws_to_stdout.await;
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-mod client;
 mod binance;
 mod bitstamp;
+mod client;
 mod data;
 
 #[tokio::main]
 async fn main() {
-    binance::do_binance().await;
-    //bitstamp::do_bitstamp().await;
+    //binance::do_binance().await;
+    bitstamp::do_bitstamp_v2().await;
 }
